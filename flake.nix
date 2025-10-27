5d893c94a3fdc047237c41d4cc4dfbbe12ff5e17
Mark Dickie <mark.dickie@gmail.com>
2025-10-27 14:16:48 +0100

    Initial commit with flake

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "Tokyo Night SDDM theme as a Nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        f pkgs
+      );
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.stdenvNoCC.mkDerivation rec {
+          pname = "tokyo-night-sddm";
+          version = "1.0";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "deephack1982";
+            repo = "tokyo-night-sddm";
+            rev = "main";
+            hash = "sha256-JRVVzyefqR2L3UrEK2iWyhUKfPMUNUnfRZmwdz05wL0=";
+          };
+
+          dontWrapQtApps = true;
+          buildInputs = with pkgs.libsForQt5.qt5; [ qtgraphicaleffects ];
+
+          installPhase =
+            let
+              iniFormat = pkgs.formats.ini { };
+              # Optionally allow overriding config via flake input
+              themeConfig = null;
+              configFile = iniFormat.generate "" { General = themeConfig; };
+
+              basePath = "$out/share/sddm/themes/tokyo-night";
+            in
+            ''
+              mkdir -p ${basePath}
+              cp -r $src/* ${basePath}
+            ''
+            + pkgs.lib.optionalString (themeConfig != null) ''
+              ln -sf ${configFile} ${basePath}/theme.conf.user
+            '';
+
+          meta = with pkgs.lib; {
+            description = "Tokyo Night SDDM theme";
+            homepage = "https://github.com/${src.owner}/${pname}";
+            license = licenses.gpl3;
+            platforms = platforms.linux;
+            maintainers = with maintainers; [ deephack1982 ];
+          };
+        };
+      });
+
+      overlays.default = final: prev: {
+        tokyo-night-sddm = self.packages.${prev.system}.default;
+      };
+    };
+}
